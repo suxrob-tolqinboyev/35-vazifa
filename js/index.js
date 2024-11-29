f37c29eb2a0595486ee5c3879fd1591944f73097
suxrob-tolqinboyev <tolqinboyevsuxrob053@gmail.com>
2024-11-30 03:44:44 +0500

    34-vazifa

---
+++ js/index.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+        width: {
+            default:"100%",
+          d: "1348px",
+        },
+        padding: {
+          default: "20px",
+        },
+      },
+      screens: {
+        d: "1348px",
+      },
+      extends: {
+        colors: {},
+      },
+    },
+  };
